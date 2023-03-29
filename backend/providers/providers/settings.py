cefdd04ac36014421dc01a64578e8a692b8f9178
bugsssssss <n.soatov@bk.ru>
2023-03-29 17:03:33 +0500

    production

--- backend/providers/providers/settings.py
+++ backend/providers/providers/settings.py
@@ -2,50 +2,25 @@ from pathlib import Path
 import pickle
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 
 # ? initialization a secret key
 # with open('secret_key.txt', 'wb') as file:
 #     pickle.dump(SECRET_KEY, file)
 
-
 # ? reading the secret key
 with open('secret_key.txt', 'rb') as file:
     SECRET_KEY = pickle.loads(file.read())
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
-# ? for development
-DEBUG = True
+DEBUG = False
 
-# ? for prodcution
-# DEBUG = False
+# ALLOWED_HOSTS = ['localhost', '91.234.219.101', 'www.internetbor.uz', 'internetbor.uz', 'http://internetbor.uz', 'http://www.internetbor.uz']
+ALLOWED_HOSTS = ['internetbor.uz']
 
-# ? for development
-ALLOWED_HOSTS = []
-
-
-# ? for production
-# ALLOWED_HOSTS = ['internetbor.uz']
-
-# ? for development
+# CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://localhost:5000", '91.234.219.101', 'http://internetbor.uz/', 'http://www.internetbor.uz']
 CORS_ALLOW_ALL_ORIGINS = True
-
-
-# ? for production
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8000", 
-#     "http://localhost:5000",
-#     '91.234.219.101', 
-#     'http://internetbor.uz/', 
-#     'http://www.internetbor.uz'
-#     ]
 
 
 # Application definition
@@ -77,6 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'providers.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -99,27 +75,25 @@ WSGI_APPLICATION = 'providers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#? for development
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-#? for production
+# ? for develepmont
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'providers',
-#         'USER': 'providersuser',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 # }
 
-
+# ? for production
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'providers',
+        'USER': 'providersuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -157,11 +131,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-#? for development
-STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
+# ? for development
+# STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
 
-#? for production
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# ? for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_RENDERER_CLASSES': (
