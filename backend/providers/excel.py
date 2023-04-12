c733872616ffbbe88f4a36fc298e58849ad6ae96
bugsssssss <n.soatov@bk.ru>
2023-04-12 15:25:57 +0500

    houses

---
+++ backend/providers/excel.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import json
+import os 
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'providers.settings')
+django.setup()
+from data.models import Coverages, AllProviders, TestCoverage
+
+
+
+def test(file_name:str):
+    df = pd.read_excel(f'../api/excel/{file_name}.xlsx')
+    grouped = df.groupby(['Город', 'Район', 'Улица'])
+    result = []
+    for name, group in grouped:
+        d = {
+            'city': name[0],
+            'district': name[1],
+            'street': name[2],
+            'providers': ['ISTV'],
+            'houses': group['Дом'].tolist()
+        }
+        result.append(d)
+    #? printing result by each object
+    # for i in result:
+    #     print(i)
+
+    print(result[1])
+    print('Total length: ', len(result))
+    with open('../api/json/istv_coverage.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=3)
+    return result
+
+
+# test('список')
+
+
+#? saving into database
+# with open('../api/json/new-overall-coverage.json') as file:
+#     data = json.load(file)
+
+# for obj in data:
+#     loc = TestCoverage(city=obj['city'], district=obj['district'], street=obj['street'], providers=obj['providers'], houses=obj['houses'])
+#     loc.save()
