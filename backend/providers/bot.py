d960461e7e0efbaed26bcdf0838e4a059192d47b
bugsssssss <n.soatov@bk.ru>
2023-05-11 17:02:27 +0500

    fix

--- backend/providers/bot.py
+++ backend/providers/bot.py
@@ -37,12 +37,18 @@ closed = types.KeyboardButton(('Закрытые 📪'))
 addressless = types.KeyboardButton(('Без адреса 🏠'))
 markup_arr = [all_requests, opened, closed, addressless]
 markup.add(all_requests, opened, closed, addressless)
-
-bot_users = requests.get('https://internetbor.uz/api/v1/bot-users').json()
+bot 
+# bot_users = requests.get('https://internetbor.uz/api/v1/bot-users').json()
+bot_users = BotUsers.objects.all()
 admin_list = []
+# for i in bot_users:
+#     if i['is_admin']:
+#         admin_list.append(int(i['user_id']))
+#     else:
+#         pass
 for i in bot_users:
-    if i['is_admin']:
-        admin_list.append(int(i['user_id']))
+    if i.is_admin:
+        admin_list.append(int(i.user_id))
     else:
         pass
 
