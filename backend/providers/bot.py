7f31f342f5c1050539a5541ef7914bdbe59ec583
bugsssssss <n.soatov@bk.ru>
2023-05-12 15:19:54 +0500

    admin list

--- backend/providers/bot.py
+++ backend/providers/bot.py
@@ -41,14 +41,15 @@ addressless = types.KeyboardButton(('Без адреса 🏠'))
 markup_arr = [all_requests, opened, closed, addressless]
 markup.add(all_requests, opened, closed, addressless)
 
-bot_users = BotUsers.objects.all()
+# bot_users = BotUsers.objects.all()
+bot_users = requests.get('https://internetbor.uz/api/v1/bot-users').json()
 admin_list = []
+
 for i in bot_users:
-    if i.is_admin:
-        admin_list.append(int(i.user_id))
+    if i['is_admin']:
+        admin_list.append(int(i['user_id']))
     else:
         pass
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
