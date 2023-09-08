289394481a338e99f07a68a1b2fe5b363dbd6eab
Fayzu1in <sherikf1@gmail.com>
2023-09-08 13:18:37 +0500

    compress bckg

--- frontend/components/HeroBackground.vue
+++ frontend/components/HeroBackground.vue
@@ -7,7 +7,7 @@ export default {}
 </script>
 <style lang="scss">
 .globalBackground {
-  background-image: url('/ib-backgroung.png');
+  background-image: url('/ib-backgroung-new.png');
 
   position: fixed;
   top: -5px;
