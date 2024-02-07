7c3b0ca81a23b2dd4bbe8cf0ff8f2cb33b25062b
bugsssssss <n.soatov@bk.ru>
2024-02-07 10:02:12 +0500

    title added to stats

--- backend/providers/data/views.py
+++ backend/providers/data/views.py
@@ -565,8 +565,6 @@ class QuickCallbackList(generics.ListCreateAPIView):
 class ClieckEventView(APIView):
     def post(self, request, format=None):
 
-        print("Request Headers: %s", request.headers)
-
         ip_address = request.META.get('REMOTE_ADDR')
         device = request.META.get('HTTP_USER_AGENT')
         
@@ -579,5 +577,7 @@ class ClieckEventView(APIView):
         
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
+            return Response({
+                "status": "success",
+            },status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
